c4a96dd4a1a8c6748357c931ad774db187dc61d9
Le Thanh Tung <tunglt042@gmail.com>
2026-03-19 00:07:52 +0700

    u

--- lib/core/constants/app_config.dart
+++ lib/core/constants/app_config.dart
@@ -2,7 +2,7 @@ class AppConfig {
   AppConfig._();
 
   // Change this to your actual API base URL
-  static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = 'https://admin-app-nestjs.vercel.app/api/v1';
   static const Duration connectTimeout = Duration(seconds: 15);
   static const Duration receiveTimeout = Duration(seconds: 15);
 }
